1070744e21eb0ad840e81b7074ab344c44b23c83
stephypy <galvanstephanie2018@gmail.com>
2020-03-17 10:52:21 -0600

    Defining states

--- project/stateMachines.c
+++ project/stateMachines.c
@@ -4,39 +4,11 @@
 #include "switches.h"
 #include "song.h"
 
-char color_state = 0;
 char changed = 0;
-char curr_state = 0;
-char state_1, state_2, state_3, state_4;
+char curr_state = -1; //Prior to attending a state
 
-char toggle_red()		/* always toggle! */
-{
-  static char state = 0;
-  switch (state) {
-  case 0:
-    red_on = 1;
-    state = 1;
-    break;
-  case 1:
-    red_on = 0;
-    state = 0;
-    break;
-  }
-  return 1;			/* always changes an led */
-}
-
-char toggle_green()	/* only toggle green if red is on!  */
-{
-  char changed = 0;
-  if (red_on) {
-    green_on ^= 1;
-    changed = 1;
-  }
-  return changed;
-}
-
-
-void state_advance()		/* change the state */
+/* Allows to Change the State  */
+void state_advance()	
 {
   /* Determine the curr state */
   switch(curr_state) {
@@ -47,17 +19,14 @@ void state_advance()		/* change the state */
 
   /* Dim Red To Bright Red */
   case 1:
-    color_state = 1;
     led_update();
 
   /* Toggle Between Red and Green */
   case 2:
-    // changed = 1;
     led_update();
 
   /* Bright Green To Dim Green */ 
   case 3:
-    color_state = 3;
     led_update();
   }
 }
--- project/stateMachines.h
+++ project/stateMachines.h
@@ -4,11 +4,9 @@
 // Current state of the MSP430
 extern char curr_state;
 
-// Will represent the initial LED color depending on the state
-extern char color_state;
-
 extern char changed;
 
+// Allows the changing of states
 void state_advance();
 
 #endif // included
