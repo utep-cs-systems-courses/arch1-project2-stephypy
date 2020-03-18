48786b92b21bf09f85cb102db8993cc05b3cdf68
stephypy <galvanstephanie2018@gmail.com>
2020-03-18 12:30:40 -0600

    Defined blink count

--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -5,13 +5,18 @@ void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
 
-  // super speedyboi fast for dimming
+  // Faster Blink Count For Dimming
   if(state == 2 || state == 4) {
+    /*
+      state 2 = dim red
+      state 4 = dim green
+     */
     if (++blink_count == 1) {
       state_advance();
       blink_count = 0;
     }
   }
+  // Regular Blink Count
   else {
     if (++blink_count == 125) {
       state_advance();
