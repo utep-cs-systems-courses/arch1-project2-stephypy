1b155b02b1909635a0dea966f8781d13c8255bca
stephypy <galvanstephanie2018@gmail.com>
2020-03-18 12:35:30 -0600

    Moved to project (:

--- project/p2_interrupt_handler.c
+++ project/p2_interrupt_handler.c
@@ -6,7 +6,7 @@ void
 __interrupt_vec(PORT2_VECTOR) Port_2(){
   if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
     P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
-    switch_interrupt_handler();	/* single handler for all switches */
+    switch_interrupt_handler();	      /* single handler for all switches */
   }
 }
 
