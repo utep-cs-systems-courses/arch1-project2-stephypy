dd3059693ec78317b4217edcea8352a17df70acc
stephypy <galvanstephanie2018@gmail.com>
2020-03-19 18:46:28 -0600

    Wrote assembly code but it doesn't work yet ;-;

--- project/src/assembly.s
+++ project/src/assembly.s
@@ -2,23 +2,23 @@
 	.p2align 1,0
 	.text
 
-	.global red_on
-	.global green_on
+	.extern red_on
+	.extern green_on
 	.globl dim_green_assembly
 dim_green_assembly:
 	sub #2, R1
 	mov.b #0, 0(R1)
 	mov.b 0(R1), R12
 	cmp #0, R12
-	JEQ #.ZERO 		; case 0
+	JEQ .ZERO 		; case 0
 	cmp #1, R12
-	JEQ #.ONE 		;case 1
+	JEQ .ONE 		;case 1
 	cmp #2, R12
-	JEQ #.TWO 		;case 2
+	JEQ .TWO 		;case 2
 	cmp #3, R12
-	JEQ #.THREE 		;case 3
+	JEQ .THREE 		;case 3
 	cmp #4, R12
-	JEQ #.FOUR 		;case 4
+	JEQ .FOUR 		;case 4
         .ZERO:
 	mov.b #0, &red_on
 	mov.b #0, &green_on
