32e0545f5a9e7b9c3ae90e8c2a8adbfa8ddf7b87
stephypy <galvanstephanie2018@gmail.com>
2020-03-23 19:30:39 -0600

    Finished (and it runs) assembly code

--- project/src/assembly.s
+++ project/src/assembly.s
@@ -3,15 +3,15 @@
 	.text
 	.global red_green_assembly
 red_green_assembly:
-	cmp.b #1, &red_on
-	jeq green
+	cmp.b #1, &red_on 	; Check whether red light is on
+	jeq green		; If red is on, go to green
 
-	mov.b #1, &red_on
-	mov.b #0, &green_on
-	jmp end
+	mov.b #1, &red_on	; Turn on red light
+	mov.b #0, &green_on	; Turn off green light
+	jmp end 		; Go to end
 
-green:	mov.b #0, &red_on
-	mov.b #1, &green_on
-	jmp end
+green:	mov.b #0, &red_on 	; Turn off red light
+	mov.b #1, &green_on 	; Turn on green light
+	jmp end 		; Go to end
 
-end:	ret
+end:	ret 			; End program
