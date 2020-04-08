3ba2808727b479b25470845bd768eb316e5f6ea1
stephypy <galvanstephanie2018@gmail.com>
2020-04-07 22:27:49 -0600

    jmp table attempt

--- project/src/assembly.s
+++ project/src/assembly.s
@@ -1,17 +1,39 @@
 	.arch msp430g2553
+
+	.data 
+light:
+	.word 1
+
+	.text
+jt:
+	.word default 		; jt[0]
+	.word red 		; jt[1]
+	.word green 		; jt[2]
+	
 	
 	.text
 	.global red_green_assembly
 red_green_assembly:
-	cmp.b #1, &red_on 	; Check whether red light is on
-	jeq green		; If red is on, go to green
+	;; why does it not work when i do the compare??
+	mov &light, r12
+	add r12, r12 		; keep it even boi
+	mov jt(r12), r0
 
-	mov.b #1, &red_on	; Turn on red light
-	mov.b #0, &green_on	; Turn off green light
-	jmp end 		; Go to end
+	;; switch statements
+green:
+	mov.b #0, &red_on
+	mov.b #1, &green_on
+	mov #0, &light
+	jmp end
+	
+red:
+	mov.b #1, &red_on
+	mov.b #0, &green_on
+	add #1, &light
+	jmp end
 
-green:	mov.b #0, &red_on 	; Turn off red light
-	mov.b #1, &green_on 	; Turn on green light
-	jmp end 		; Go to end
+default:
+	mov #1, &light
 
-end:	ret 			; End program
+end:
+	pop r0
